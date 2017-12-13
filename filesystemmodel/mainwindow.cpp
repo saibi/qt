1f540528a8c22c061e04ba529a5597d401e2c535
Youngmin Kim <ymkim@huvitz.com>
2017-12-13 09:05:32 +0900

    filesystemmodel : reimplement double click to navigate dir

--- filesystemmodel/mainwindow.cpp
+++ filesystemmodel/mainwindow.cpp
@@ -1,20 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileInfo>
+
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
 
+	m_cwd = "/home/saibi";
+
 	m_dirModel = new QFileSystemModel(this);
-	m_dirModel->setRootPath("/home/saibi");
-	m_dirModel->setFilter(QDir::AllDirs | QDir::Files);
+	m_dirModel->setRootPath("/");
+	m_dirModel->setFilter(QDir::AllDirs | QDir::Files | QDir::NoDot);
 	ui->treeView->setModel(m_dirModel);
-	ui->treeView->setRootIndex( m_dirModel->index("/home/saibi"));
+	ui->treeView->setRootIndex( m_dirModel->index(m_cwd));
+
 }
 
 MainWindow::~MainWindow()
 {
 	delete ui;
+}
+
+void MainWindow::on_treeView_doubleClicked(const QModelIndex &index)
+{
+	qDebug("[%s] start", Q_FUNC_INFO);
+
+	QFileInfo f = m_dirModel->fileInfo(index);
+
+	if ( f.isDir() )
+	{
+		m_cwd = f.absoluteFilePath();
+		qDebug("[%s] %s", Q_FUNC_INFO, qPrintable(f.absoluteFilePath()));
+
+		ui->treeView->setRootIndex(m_dirModel->index(m_cwd) );
+	}
 }
--- filesystemmodel/mainwindow.h
+++ filesystemmodel/mainwindow.h
@@ -16,10 +16,15 @@ public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
 
+private slots:
+	void on_treeView_doubleClicked(const QModelIndex &index);
+
 private:
 	Ui::MainWindow *ui;
 
 	QFileSystemModel *m_dirModel;
+
+	QString m_cwd;
 };
 
 #endif // MAINWINDOW_H
