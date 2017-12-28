274c284b01ef81621ad7e8af8449d51e9588a928
Youngmin Kim <ymkim@huvitz.com>
2017-12-28 11:04:00 +0900

    filesystemmodel : remove dbg msg

--- filesystemmodel/kbdtreeview.cpp
+++ filesystemmodel/kbdtreeview.cpp
@@ -8,7 +8,6 @@ KbdTreeView::KbdTreeView(QWidget *parent) : QTreeView(parent)
 
 void KbdTreeView::keyPressEvent(QKeyEvent *e)
 {
-	qDebug("[%s] %d", Q_FUNC_INFO, e->key());
 	QTreeView::keyPressEvent(e);
 
 	emit keyPressed(e->key());
