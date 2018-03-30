7ba390a7d06ba43623ba951a00a817817f39b699
Youngmin Kim <ymkim@huvitz.com>
2018-03-30 18:05:41 +0900

    cpe4 screenshot dump viewer

---
+++ sc_dump_viewer/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+
+private slots:
+	void on_actionExit_triggered();
+
+	void on_actionOpen_triggered();
+
+private:
+	Ui::MainWindow *ui;
+
+	QString m_prevFile;
+
+
+	QVector <QRgb> m_colorTable;
+
+};
+
+#endif // MAINWINDOW_H
