6dfaee50e141df88292303c9a20736db72ea9db7
Youngmin Kim <ymkim@huvitz.com>
2018-07-19 16:17:28 +0900

    empty qt widget - dump elapsed time

---
+++ empty_app/main.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+#include <QApplication>
+#include <QTime>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+
+	QTime t;
+	t.start();
+
+	qDebug("DBG before widget %d", t.elapsed());
+
+	Widget w;
+	qDebug("DBG before show %d", t.elapsed());
+
+	w.show();
+
+	qDebug("DBG before a.exec %d", t.elapsed());
+
+	return a.exec();
+}
