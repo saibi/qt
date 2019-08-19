5e3f3283549d0bba4233f98885f09c401b55ef2c
Youngmin Kim <ymkim@huvitz.com>
2019-08-19 13:34:45 +0900

    qdebug() <<

--- console_mem/main.cpp
+++ console_mem/main.cpp
@@ -3,6 +3,21 @@
 #include <QDebug>
 #include <QDateTime>
 
+
+struct Person {
+	QString name;
+	int age;
+};
+
+
+QDebug operator<< (QDebug debug, const Person & person)
+{
+	QDebugStateSaver saver(debug);
+
+	debug.nospace() << "(" << "name: " << person.name << ", " << "age: " << person.age << ")";
+	return debug;
+}
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
@@ -23,6 +38,10 @@ int main(int argc, char *argv[])
 	qsrand(QDateTime::currentDateTime().toTime_t());
 	qDebug() << qrand() % 10 << qrand() % 10;
 
+
+	Person person = { "Lenna", 64 };
+
+	qDebug() << "Person info" << person;
 
 	return a.exec();
 }
