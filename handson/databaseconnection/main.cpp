749add6acd6aae3a11c28085e83d22b5b6ee2f93
Youngmin Kim <ymkim@huvitz.com>
2019-11-18 13:11:53 +0900

    sql command test

--- handson/databaseconnection/main.cpp
+++ handson/databaseconnection/main.cpp
@@ -25,5 +25,40 @@ int main(int argc, char *argv[])
 		qDebug() << "Failed to connect.";
 		return 0;
 	}
+
+
+	{
+		QString command = "SELECT name FROM department";
+		QSqlQuery query(db);
+		if ( query.exec(command) )
+		{
+			while(query.next())
+			{
+				QString name = query.value("name").toString();
+				qDebug() << name;
+
+			}
+		}
+	}
+
+	{
+		QString command = "SELECT my_user.username, department.name AS deptname FROM (SELECT * FROM user WHERE status = 0) AS my_user INNER JOIN department ON department.id = my_user.deptID";
+		QSqlQuery query(db);
+
+		if ( query.exec(command) )
+		{
+			while(query.next())
+			{
+				QString username = query.value("username").toString();
+				QString department = query.value("deptname").toString();
+
+
+				qDebug() << username << department;
+
+			}
+		}
+
+	}
+
 	return a.exec();
 }
