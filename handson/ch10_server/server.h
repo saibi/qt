84b9d815c7e0fe68d806906b79e5b10bcff09d4b
Youngmin Kim <ymkim@huvitz.com>
2019-11-22 14:55:24 +0900

    chapter 10 tcp server

---
+++ handson/ch10_server/server.h
@@ -0,0 +1,33 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QDebug>
+#include <QVector>
+
+
+class Server : public QObject
+{
+	Q_OBJECT
+
+
+public:
+	explicit Server(QObject *parent = nullptr);
+
+	void startServer();
+	void sendMessageToClients(QString message);
+
+public slots:
+	void newClientConnection();
+	void socketDisconnected();
+	void socketReadyRead();
+	void socketStateChanged(QAbstractSocket::SocketState state);
+
+private:
+	QTcpServer * chatServer;
+	QVector<QTcpSocket*>* allClients;
+};
+
+#endif // SERVER_H
