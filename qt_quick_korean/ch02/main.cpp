e3e0e531f58bbb7d94e429147eea15a51c5de535
Youngmin Kim <kimyoungmin@gmail.com>
2021-02-02 18:18:12 +0900

    quick_korean: ch02 calendar

--- qt_quick_korean/ch02/main.cpp
+++ qt_quick_korean/ch02/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
-	engine.load(QUrl(QStringLiteral("qrc:/positioner.qml")));
+	engine.load(QUrl(QStringLiteral("qrc:/calendar.qml")));
 	if (engine.rootObjects().isEmpty())
 		return -1;
 
