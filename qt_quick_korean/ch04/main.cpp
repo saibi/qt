901f9f114047d94cbedae18659b2a08a841ca8d7
Youngmin Kim <kimyoungmin@gmail.com>
2021-02-03 16:26:18 +0900

    quick_korean:ch04 XmlListModel

--- qt_quick_korean/ch04/main.cpp
+++ qt_quick_korean/ch04/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
-	const QUrl url(QStringLiteral("qrc:/gridview.qml"));
+	const QUrl url(QStringLiteral("qrc:/xmllistmodel.qml"));
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
 					 &app, [url](QObject *obj, const QUrl &objUrl) {
 		if (!obj && url == objUrl)
