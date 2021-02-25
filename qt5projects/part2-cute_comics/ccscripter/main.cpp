b8482b225af598b6c4db703b33c69f898bc449a4
Youngmin Kim <kimyoungmin@gmail.com>
2021-02-25 10:29:21 +0900

    qt5projects:ch06 pdf

--- qt5projects/part2-cute_comics/ccscripter/main.cpp
+++ qt5projects/part2-cute_comics/ccscripter/main.cpp
@@ -10,6 +10,8 @@
 #include <QFile>
 #include <QMessageBox>
 #include <QDebug>
+#include <QFileInfo>
+#include <QPrinter>
 
 #include "ui_mainwindow.h"
 
@@ -26,6 +28,7 @@ int main(int argc, char *argv[])
 	auto addCharacterInput = w.ui->lineEdit_addCharacter;
 	auto scriptEditor = w.ui->textEdit_scriptEditor;
 	auto actionSaveAs = w.ui->actionSaveAs;
+	auto actionExportPdf = w.ui->actionExportPdf;
 
 	if (scriptEditor)
 		new ScriptHighlighter(scriptEditor->document());
@@ -67,7 +70,31 @@ int main(int argc, char *argv[])
 					return;
 				}
 				QDataStream out(&file);
-				out << scriptEditor->toPlainText();
+				out << scriptEditor->toPlainText().toUtf8();
+			}
+		});
+	}
+
+	if ( actionExportPdf && scriptEditor )
+	{
+		QObject::connect(actionExportPdf, &QAction::triggered, [scriptEditor]() {
+			QString fileName = QFileDialog::getSaveFileName();
+			if ( !fileName.isEmpty() )
+			{
+				if ( QFileInfo(fileName).suffix().isEmpty() )
+				{
+					fileName.append(".pdf");
+				}
+
+				QPrinter printer(QPrinter::PrinterResolution);
+				printer.setOutputFormat(QPrinter::PdfFormat);
+				printer.setPaperSize(QPrinter::A4);
+				printer.setOutputFileName(fileName);
+				printer.setFontEmbeddingEnabled(true);
+
+				auto doc = scriptEditor->document();
+				doc->print(&printer);
+
 			}
 		});
 	}
