d98a6c1644e451f5fafc1962c40b69744ba07b17
Youngmin Kim <kimyoungmin@gmail.com>
2021-03-11 08:49:53 +0900

    handson embedded qt:ch04 QVariant

--- handson_embedded_qt/ch04_ConsoleTimers/main.cpp
+++ handson_embedded_qt/ch04_ConsoleTimers/main.cpp
@@ -2,6 +2,7 @@
 
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -19,6 +20,23 @@ int main(int argc, char *argv[])
 	});
 	tocTimer.start();
 
+
+	QVariant v;
+	v.setValue(10);
+
+	int y = 41 * v.toInt();
+	qDebug() << v.toString() << " * 41 = " << y;
+	qDebug() << "v as a bool = " << v.toBool();
+	qDebug() << "v as a list = " << v.toList();
+
+
+	QList<QVariant> l;
+	l << v;
+	l << QVariant(1);
+	l << QVariant(2.3);
+	l << QVariant("Hello World");
+
+	qDebug() << "The list: " << l;
 
 	return a.exec();
 }
